e697e7ddfe73a181547c7a4ed54aedce66e0d8df
Jonathan Heinzman <Jond.heinzman@icloud.com>
2026-06-20 13:41:31 -0600

    Initial Commit

---
+++ ShoppingList/ShoppingListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingListApp.swift
+//  ShoppingList
+//
+//  Created by Jonathan Heinzman on 6/20/26.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
